3be105128863b8d342fe6d3dd8f2e4a201c69355
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-05-09 15:49:59 -0700

    add coding challege 04

---
+++ Week 1 /04.ReverseString.js
@@ -0,0 +1,16 @@
+/*
+reverseString.js - Write a function that takes a string as input and returns the string reversed.
+reverseString("never") // returns "reven"
+*/
+
+/*
+    1 What are we given?
+  Inputs; A string that could be any size.
+  Outputs: Another string that is going to be reversed (as in the example)
+  Constraints: The only constraint I see is the string not being a string. So we might need to incorporate this into the code, so we can exclude nonstring parameters.
+*/
+
+
+function reverseString(str){
+
+}
