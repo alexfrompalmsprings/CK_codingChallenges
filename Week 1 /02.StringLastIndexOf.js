eb6c00f43e25b472e213476b1dbe1b9dd8ce5830
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-05-09 15:52:53 -0700

    add question #02

---
+++ Week 1 /02.StringLastIndexOf.js
@@ -0,0 +1,12 @@
+// Write a function called stringLastIndexOf, which accepts two strings: the first is a word and the second is a single character.
+
+// The function should return the last index at which the character exists or -1 if the character is not found.
+// Do not use the built in String.lastIndexOf() function!
+
+function stringLastIndexOf(str, char) {
+
+}
+
+// Examples:
+// stringLastIndexOf('awesome', 'e'); // 6
+// stringLastIndexOf('awesome', 'z'); // -1
