aaf09b5edf96eb063fbaf252dd30a504c9c4c0de
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-05-09 15:51:24 -0700

    add challenge #03

---
+++ Week 1 /03.Intersection.js
@@ -0,0 +1,13 @@
+/*
+  Write a function, intersection, that takes two arrays and returns
+  the elements that are in both arrays.
+  var a = [4, 1, 2, 3];
+  var b = [3, 7, 5, 1, 9, 8];
+  intersection(a, b)
+  should return [1, 3]
+*/
+
+
+var intersection = function(arrA, arrB) {
+
+};
