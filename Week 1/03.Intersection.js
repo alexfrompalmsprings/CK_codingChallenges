328e499c1b130d59067082535baf9e05d0ef462c
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-05-10 17:46:45 -0700

    solve intersection.js

--- Week 1/03.Intersection.js
+++ Week 1/03.Intersection.js
@@ -7,7 +7,31 @@
   should return [1, 3]
 */
 
-
 var intersection = function(arrA, arrB) {
 
+  let result = [];
+  let objectB = {}
+
+  for(let i = 0; i < arrB.length; i++){
+    let currentNum = arrB[i]
+
+    if(objectB[currentNum] === undefined){
+      objectB[currentNum] = true;
+    }
+    // console.log(objectB)
+  }
+
+  for(let j =0; j < arrA.length; j++){
+    let currentNumA = arrA[j]
+
+    if(objectB[currentNumA]){
+      result.push(currentNumA)
+    }
+    // console.log(result)
+  }
+
+
+  return result;
 };
+
+console.log(intersection([4, 1, 2, 3],[3, 7, 5, 1, 9, 8]))
