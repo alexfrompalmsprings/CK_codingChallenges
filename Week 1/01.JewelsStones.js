5a5cd6841e5afc8f6775a16ab18fba9f94fecc64
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-05-10 17:11:03 -0700

    change jewels and stones

--- Week 1/01.JewelsStones.js
+++ Week 1/01.JewelsStones.js
@@ -1,14 +1,30 @@
-// Write a function called stringLastIndexOf, which accepts two strings: the first is a word and the second is a single character.
-
-// The function should return the last index at which the character exists or -1 if the character is not found.
-// Do not use the built in String.lastIndexOf() function!
-
-function stringLastIndexOf(str, char) {
+// 771. Jewels and Stones
 
 
+// Share
+// You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
-}
+// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
-// Examples:
-// stringLastIndexOf('awesome', 'e'); // 6
-// stringLastIndexOf('awesome', 'z'); // -1
+// Example 1:
+
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: J = "z", S = "ZZ"
+// Output: 0
+// Note:
+
+// S and J will consist of letters and have length at most 50.
+// The characters in J are distinct.
+
+
+var numJewelsInStones = function(J, S) {
+
+
+
+
+
+
+};
