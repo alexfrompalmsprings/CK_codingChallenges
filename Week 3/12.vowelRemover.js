0ef629802d27ebec55d301374a9b7f58fe60ed58
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-05-20 20:25:32 -0700

    create question #12 vowelRemover

---
+++ Week 3/12.vowelRemover.js
@@ -0,0 +1,14 @@
+// Vowel remover
+
+// Create a function called shortcut to remove all the lowercase vowels in a given string.
+// Don't worry about uppercase vowels.
+
+function shortcut(string){
+
+
+
+}
+
+// Examples
+// shortcut("codewars") // --> cdwrs
+// shortcut("goodbye")  // --> gdby
