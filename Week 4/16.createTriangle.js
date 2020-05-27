e94cba8c758dcd65c2154dddfbdd4588595f3659
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-05-26 18:25:13 -0700

    update challenge question #16

---
+++ Week 4/16.createTriangle.js
@@ -0,0 +1,14 @@
+// Creating a triangle
+// Write a function that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+
+
+triangle();
