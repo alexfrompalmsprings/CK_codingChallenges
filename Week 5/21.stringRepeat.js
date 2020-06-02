ae5f43ca21adb9b876925e0d9e833b96ce9f3298
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-06-01 20:12:49 -0700

    week 5 - question #21 - stringRepeat.js

---
+++ Week 5/21.stringRepeat.js
@@ -0,0 +1,13 @@
+// String repeat
+
+// Instructions
+// Write a function called repeatString which repeats the given String src exactly count times.
+
+function repeatStr (n, s) {
+
+}
+
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+// FUNDAMENTALS
