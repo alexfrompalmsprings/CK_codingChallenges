b6deaeb91ae10cc21e6e5cd67dc53bc007065d70
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-06-16 17:04:28 -0700

    add Counting Cars to meetup 02

---
+++ MeetUps/MeetUp 02 /invertTheArray.js
@@ -0,0 +1,18 @@
+// Invert values
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+
+function invert(array) {
+
+}
+
+
+console.log(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
+console.log(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
+console.log(invert([]), []);
+console.log(invert([0]), [0]);
