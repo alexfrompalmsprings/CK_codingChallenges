d199632d3778ac2e8039d701eee8da1169f891dc
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 19:44:30 -0700

    add largestSwap to the meetup folder

---
+++ MeetUps/MeetUp 08/largestSwap.js
@@ -0,0 +1,11 @@
+//write a function that takes a two-digit number and
+//determine if its largest of two possibilities digit swaps
+
+let largestSwap = (num) => {
+
+
+}
+
+console.log(largestSwap(14))
+console.log(largestSwap(53))
+console.log(largestSwap(99))
