9efeeece8db08c0780e97b5baca97c3496459759
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 20:56:24 -0700

    solution #2 negateNUMS

--- MeetUps/MeetUp 08/negateTheArrayNums.js
+++ MeetUps/MeetUp 08/negateTheArrayNums.js
@@ -2,14 +2,21 @@
 
 let negate = (numbers) => {
   // solution #1
-  let result = numbers;
+  // let result = numbers;
 
-  for(let i = 0; i < result.length; i++){
-    result[i] *= -1;
-  }
+  // for(let i = 0; i < result.length; i++){
+  //   result[i] *= -1;
+  // }
 
-  return result;
+  // return result;
 
+
+  //solution #2
+let result = numbers;
+
+ result.forEach((num) => num *= -1);
+
+ return result;
 }
 
 console.log(negate([1, 2, 3, 4]))
