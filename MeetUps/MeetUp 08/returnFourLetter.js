000dc8d3ac87602c43285d0fa02fd8a41765a9a9
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 19:29:46 -0700

    add fourLetters to the meetUp 08

---
+++ MeetUps/MeetUp 08/returnFourLetter.js
@@ -0,0 +1,9 @@
+//create a function that takes an array of strings and returns the words that are exactly four letters
+
+let fourLetters = (arr) => {
+
+}
+
+console.log(fourLetters(['Tomato', 'Potato', 'Pair']))
+console.log(fourLetters(['Kangaroo', 'Bear', 'Fox']))
+fourLetters(['Ryan', 'Kieran', 'Jason', 'Matt'])
