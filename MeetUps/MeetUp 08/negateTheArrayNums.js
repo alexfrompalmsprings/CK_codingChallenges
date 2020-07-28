a6c52a8797b01e7fe47021ca93834b5c1119b6de
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 20:53:12 -0700

    solution #1 - negate the array nums

--- MeetUps/MeetUp 08/negateTheArrayNums.js
+++ MeetUps/MeetUp 08/negateTheArrayNums.js
@@ -1,10 +1,17 @@
 //Given an array of numbers, negate all elements contained within
 
-let negate = (nums) => {
+let negate = (numbers) => {
+  // solution #1
+  let result = numbers;
 
+  for(let i = 0; i < result.length; i++){
+    result[i] *= -1;
+  }
+
+  return result;
 
 }
 
-console.log(negate([1,2,3,4]))
-console.log(negate([-1,2,-3,4]))
-console.log(negate(-1,-2,-3,-4))
+console.log(negate([1, 2, 3, 4]))
+console.log(negate([-1, 2, -3, 4]))
+console.log(negate([-1, -2, -3, -4]))
