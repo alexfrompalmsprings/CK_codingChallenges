02756761ffd01d12df994f08bab91d0d1094e370
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 21:32:44 -0700

    complete largestSwap

--- MeetUps/MeetUp 08/largestSwap.js
+++ MeetUps/MeetUp 08/largestSwap.js
@@ -1,8 +1,30 @@
 //write a function that takes a two-digit number and
 //determine if its largest of two possibilities digit swaps
 
+
+let helperFunc = (num) => {
+
+  let reversed = num.toString().split('').reverse().join('')
+
+  let toNum = Number(reversed);
+
+  return toNum;
+}
+
+// console.log(helperFunc(14))
+
+
+
 let largestSwap = (num) => {
 
+  let original = num;
+  let comparison = num;
+
+  let swapped = helperFunc(comparison);
+
+
+  // console.log(original, swapped)
+  return original >= swapped;
 
 }
 
