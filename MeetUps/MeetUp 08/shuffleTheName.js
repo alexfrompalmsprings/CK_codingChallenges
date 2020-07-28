8ffde5ae524f43581d61428fa6eb007651de0828
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 19:21:13 -0700

    add the shuffle Name question

---
+++ MeetUps/MeetUp 08/shuffleTheName.js
@@ -0,0 +1,9 @@
+// Create a function that accepts a string
+// and returns a string with the first and last name swapped
+
+let shuffleName = (string) => {
+
+
+}
+
+console.log(shuffleName('Alex Avila'))
