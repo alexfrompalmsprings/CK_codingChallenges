bbafa495c05a88f19727cc651e65803d01e80938
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 19:38:04 -0700

    add negate nums

---
+++ MeetUps/MeetUp 08/negateTheArrayNums.js
@@ -0,0 +1,10 @@
+//Given an array of numbers, negate all elements contained within
+
+let negate = (nums) => {
+
+
+}
+
+console.log(negate([1,2,3,4]))
+console.log(negate([-1,2,-3,4]))
+console.log(negate(-1,-2,-3,-4))
