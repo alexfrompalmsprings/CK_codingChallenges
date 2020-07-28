07f0ac3aaa98b16054f18c9cc4f6f3669b9f6bd3
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-07-27 19:41:41 -0700

    add sevenBoom to the meetup folder

---
+++ MeetUps/MeetUp 08/sevenBoom.js
@@ -0,0 +1,14 @@
+// create a function that takes an array numbers
+// and return BOOM if the number 7 appears in the array
+
+let sevenBoom = (arr) => {
+
+
+
+
+
+}
+
+console.log(sevenBoom([1,2,3,4,5,6,7]))
+console.log(sevenBoom([8,6,33,100]))
+console.log(sevenBoom([2,55,60,97,86]))
