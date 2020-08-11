63dfdced4269bec1859f2691fac09116efc12d79
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-08-10 20:59:10 -0700

    added questions for meetup 10

---
+++ MeetUps/MeetUp 10/allProblems.js
@@ -0,0 +1,86 @@
+// Link: https://repl.it/@alexisaviles/MeetUp10#index.js
+
+/*
+ ______ _          _     _____
+|  ____(_)        | |   |  __ \
+| |__   _ _ __ ___| |_  | |__) |_____   _____ _ __ ___  ___
+|  __| | | '__/ __| __| |  _  // _ \ \ / / _ \ '__/ __|/ _ \
+| |    | | |  \__ \ |_  | | \ \  __/\ V /  __/ |  \__ \  __/
+|_|    |_|_|  |___/\__| |_|  \_\___| \_/ \___|_|  |___/\___|
+
+
+First Reverse
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example:
+
+if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
+Examples
+Input: "coderbyte"
+Output: etybredoc
+Input: "I Love Code"
+Output: edoC evoL I
+
+*/
+
+
+/*
+ ______ _          _     ______         _             _       _
+|  ____(_)        | |   |  ____|       | |           (_)     | |
+| |__   _ _ __ ___| |_  | |__ __ _  ___| |_ ___  _ __ _  __ _| |
+|  __| | | '__/ __| __| |  __/ _` |/ __| __/ _ \| '__| |/ _` | |
+| |    | | |  \__ \ |_  | | | (_| | (__| || (_) | |  | | (_| | |
+|_|    |_|_|  |___/\__| |_|  \__,_|\___|\__\___/|_|  |_|\__,_|_|
+
+
+First Factorial
+Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+Examples
+Input: 4
+Output: 24
+Input: 8
+Output: 40320
+*/
+
+/*
+ _                                 _    __          __           _
+| |                               | |   \ \        / /          | |
+| |     ___  _ __   __ _  ___  ___| |_   \ \  /\  / /__  _ __ __| |
+| |    / _ \| '_ \ / _` |/ _ \/ __| __|   \ \/  \/ / _ \| '__/ _` |
+| |___| (_) | | | | (_| |  __/\__ \ |_     \  /\  / (_) | | | (_| |
+|______\___/|_| |_|\__, |\___||___/\__|     \/  \/ \___/|_|  \__,_|
+                    __/ |
+                   |___/
+
+
+
+Longest Word
+Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+Examples
+Input: "fun&!! time"
+Output: time
+Input: "I love dogs"
+Output: love
+*/
+
+/*
+ _          _   _               _____            _ _        _ _
+| |        | | | |             / ____|          (_) |      | (_)
+| |     ___| |_| |_ ___ _ __  | |     __ _ _ __  _| |_ __ _| |_ _______
+| |    / _ \ __| __/ _ \ '__| | |    / _` | '_ \| | __/ _` | | |_  / _ \
+| |___|  __/ |_| ||  __/ |    | |___| (_| | |_) | | || (_| | | |/ /  __/
+|______\___|\__|\__\___|_|     \_____\__,_| .__/|_|\__\__,_|_|_/___\___|
+                                          | |
+                                          |_|
+
+Letter Capitalize
+Have the function LetterCapitalize(str) take the str parameter
+being passed and capitalize the first letter of each word.
+Words will be separated by only one space.
+*/
+
+
+
+
+
+
+
