745d176a04c604bb957cb650d40cda621fcc230a
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-08-11 17:27:48 -0700

    small logic of the question

--- MeetUps/MeetUp 10/allProblems.js
+++ MeetUps/MeetUp 10/allProblems.js
@@ -175,6 +175,15 @@ function fizzbuzz(n) {
   }
 
   return result;
+
+
+  // the logic of the problem
+  //   for (var i=1; i < 101; i++){
+  //     if (i % 15 == 0) console.log("FizzBuzz");
+  //     else if (i % 3 == 0) console.log("Fizz");
+  //     else if (i % 5 == 0) console.log("Buzz");
+  //     else console.log(i);
+  // }
 }
 
 
