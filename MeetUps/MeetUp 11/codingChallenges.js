869c8a2d0ed6881889b888787e36da9ed7175606
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-08-16 13:57:02 -0700

    sum max & min

--- MeetUps/MeetUp 11/codingChallenges.js
+++ MeetUps/MeetUp 11/codingChallenges.js
@@ -36,3 +36,23 @@ function DNAtoRNA(dna) {
   // create a function which returns an RNA sequence from the given DNA sequence
 
 }
+
+
+/*  _____                            __  __          __   __      __  __ _____ _   _
+ / ____|                          |  \/  |   /\    \ \ / /___  |  \/  |_   _| \ | |
+| (___  _   _ _ __ ___    ______  | \  / |  /  \    \ V /( _ ) | \  / | | | |  \| |
+ \___ \| | | | '_ ` _ \  |______| | |\/| | / /\ \    > < / _ \/\ |\/| | | | | . ` |
+ ____) | |_| | | | | | |          | |  | |/ ____ \  / . \ (_>  < |  | |_| |_| |\  |
+|_____/ \__,_|_| |_| |_|          |_|  |_/_/    \_\/_/ \_\___/\/_|  |_|_____|_| \_|
+
+
+
+Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+(The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+*/
+
+function sumArray(array) {
+
+
+}
+console.log(sumArray([ 6, 2, 1, 8, 10 ]))
