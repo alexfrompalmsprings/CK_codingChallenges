8679787c49c7dfb578ca2456e24d1050d77f7e23
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-08-16 13:45:07 -0700

    add fakeBin

---
+++ MeetUps/MeetUp 11/codingChallenges.js
@@ -0,0 +1,17 @@
+/*
+  ___      _          ______  _
+ / __)    | |        (____  \(_)
+| |__ ____| |  _ ____ ____)  )_ ____
+|  __) _  | | / ) _  )  __  (| |  _ \
+| | ( ( | | |< ( (/ /| |__)  ) | | | |
+|_|  \_||_|_| \_)____)______/|_|_| |_|
+
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+*/
+
+function fakeBin(string){
+
+
+}
+
