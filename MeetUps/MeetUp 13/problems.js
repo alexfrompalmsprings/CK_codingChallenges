6631e27c0da324f8f9142c47920cbe7a0239b688
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-08-31 21:17:52 -0700

    comlete contains vowel using a loop

--- MeetUps/MeetUp 13/problems.js
+++ MeetUps/MeetUp 13/problems.js
@@ -9,7 +9,7 @@ __   _______      _____| |    ) | |  ) |
 
 // create a function that tell us if the letter is a vowel
 
-let areYouAvowel = function(letter){
+let areYouAvowel = function (letter) {
 
   let lowered = letter.toLowerCase();
 
@@ -19,3 +19,45 @@ let areYouAvowel = function(letter){
 console.log(areYouAvowel('a'))
 console.log(areYouAvowel('b'))
 console.log(areYouAvowel('E'))
+
+
+
+/*
+                 _        _                                    _
+                | |      (_)                                  | |
+  ___ ___  _ __ | |_ __ _ _ _ __  ___  __   _______      _____| |___
+ / __/ _ \| '_ \| __/ _` | | '_ \/ __| \ \ / / _ \ \ /\ / / _ \ / __|
+| (_| (_) | | | | || (_| | | | | \__ \  \ V / (_) \ V  V /  __/ \__ \
+ \___\___/|_| |_|\__\__,_|_|_| |_|___/   \_/ \___/ \_/\_/ \___|_|___/
+                                                                                                                                        */
+// create a function that returns true/false depending if the word includes a vowel
+
+let doYouHaveVowels = function (word) {
+
+  // let vowels = 'aeiou'
+
+  // two ways to do this right?!?
+
+  // solution #1 - for loop
+
+  for (let i = 0; i < word.length; i++) {
+    let lowered = word[i].toLowerCase();
+    console.log(lowered)
+
+    if (lowered === 'a' || lowered === 'e' || lowered === 'i' || lowered === 'o' || lowered === 'u') {
+
+      return true
+
+    }
+
+  }
+
+
+  return false;
+
+}
+console.log(doYouHaveVowels('alex'))
+console.log(doYouHaveVowels('lexi'))
+console.log(doYouHaveVowels('myths'))
+console.log(doYouHaveVowels('psych'))
+console.log(doYouHaveVowels('rhythms'))
