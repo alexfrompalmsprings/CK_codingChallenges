bc2229f3ac6323e763b9a618fbb1ed517d04709e
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-09-28 18:57:38 -0700

    complete filtered

--- MeetUps/MeetUp 17/problems.js
+++ MeetUps/MeetUp 17/problems.js
@@ -25,8 +25,25 @@
 // Write a function called "filterEvenLengthWords".
 // Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the given array whose length is an even number.
 
+function evenFinder(word) {
+  return word.length % 2 === 0 ? true : false;
+}
+
+console.log(evenFinder('alex'));
+console.log(evenFinder('alexa'));
 
 
+
+function filterEvenLengthWords(arr) {
+
+  let filtered = arr.filter((word) => {
+    return evenFinder(word)
+  })
+
+  return filtered;
+
+}
+console.log(filterEvenLengthWords(['One', 'Two', 'Three', 'Four', 'Alex']));
 
 
 
