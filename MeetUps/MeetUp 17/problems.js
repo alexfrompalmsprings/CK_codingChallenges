8343195544848c2f836b3ef07ed3e947f28303b2
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-09-28 18:36:23 -0700

    add question filter

---
+++ MeetUps/MeetUp 17/problems.js
@@ -0,0 +1,34 @@
+/*
+                                               _
+                                              | |
+  __ _ _ __ _ __ __ _ _   _   _ __   __ _ _ __| |_ _   _
+ / _` | '__| '__/ _` | | | | | '_ \ / _` | '__| __| | | |
+| (_| | |  | | | (_| | |_| | | |_) | (_| | |  | |_| |_| |
+ \__,_|_|  |_|  \__,_|\__, | | .__/ \__,_|_|   \__|\__, |
+                       __/ | | |                    __/ |
+                      |___/  |_|                   |___/
+*/
+
+
+
+/*
+  __ _ _ _
+ / _(_) | |
+| |_ _| | |_ ___ _ __
+|  _| | | __/ _ \ '__|
+| | | | | ||  __/ |
+|_| |_|_|\__\___|_|
+ */
+
+
+
+// Write a function called "filterEvenLengthWords".
+// Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the given array whose length is an even number.
+
+
+
+
+
+
+
+// var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
