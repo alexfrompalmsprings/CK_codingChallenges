c8d1176db3ea4394274fcb9857f64edd4df191f0
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-10-19 19:30:15 -0700

    add questions

---
+++ MeetUps/MeetUp 20/problems.js
@@ -0,0 +1,61 @@
+// --- wordsCombination
+// given two strings, write a function to determine if string 2 can be created with the letters of the string 1
+
+function wordsCombo(str1, str2) {
+
+}
+
+// console.log(wordsCombo('anagram', 'anna'))
+// console.log(wordsCombo('cinema', 'iceman'))
+// console.log(wordsCombo('cinema', 'ireman'))
+
+
+
+// --- words Combinations Counter
+// Given two strings, write a function to determine if strings 2 can be created with the letters of the string 1. Make sure you
+
+function freqCounter(arr) {
+
+}
+
+// console.log(freqCounter([2,2,2,4,4,4,5,5,5,10]))
+
+
+function anagramHash(arr1, arr2) {
+
+}
+
+// console.log(anagramHash('anagram', 'xanna'))
+// console.log(anagramHash('cinema', 'iceman'))
+
+
+// --- averagePairLoop
+
+// write a function called averagePair. Given a sorted array of integers and a target average,
+// determine if there is a pair ofa values in the array where the average equals the target value.
+// There maybe one more than one pair that matches the average target.
+
+function averagePairLoop(arr, target) {
+
+}
+console.log(averagePairLoop([1, 2, 3], 2.5)) // true
+console.log(averagePairLoop([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)) //true
+console.log(averagePairLoop([-1, 0, 3, 4, 5, 6], 4.1)) // false
+console.log(averagePairLoop([], 4)) //false
+
+
+
+
+// averagePair
+
+// write a function called averagePair. Given a sorted array of integers and a target average,
+// determine if there is a pair ofa values in the array where the average equals the target value.
+// There maybe one more than one pair that matches the average target.
+
+function averagePair(arr, target) {
+
+}
+// console.log(averagePair([1, 2, 3], 2.5)) // true
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)) //true
+// console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1))
+// console.log(averagePair([], 4))
