97fa17620b464ad359a4ffbb03e913dbc219bba1
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-11-30 17:06:33 -0800

    add exercise #2 - meetup 24 - reduce

--- MeetUps/MeetUp 24/problems.js
+++ MeetUps/MeetUp 24/problems.js
@@ -26,6 +26,57 @@ console.log(classicSum(numbers));
 // using reduce
 let reduceSum = numbers.reduce(function (total, number) {
   return total + number;
-}, classicSum(numbers));
+}, classicSum(numbers)); // passing in a function here
 
 console.log(reduceSum);
+
+
+// _     _           _           __          _
+// ___ | |__ (_) ___  ___| |_ ___    /__\ ___  __| |_   _  ___ ___
+// / _ \| '_ \| |/ _ \/ __| __/ __|  / \/// _ \/ _` | | | |/ __/ _ \
+// | (_) | |_) | |  __/ (__| |_\__ \ / _  \  __/ (_| | |_| | (_|  __/
+// \___/|_.__// |\___|\___|\__|___/ \/ \_/\___|\__,_|\__,_|\___\___|
+//         |__/
+
+let studentsInfo = [{
+    name: 'alex',
+    location: 'CA',
+    system: 'mac'
+  },
+  {
+    name: 'katie',
+    location: 'WA',
+    system: 'mac'
+  },
+  {
+    name: 'nas',
+    location: 'CA',
+    system: 'windows'
+  },
+  {
+    name: 'drew',
+    location: 'DC',
+    system: 'windows'
+  },
+];
+
+function namesFunction(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let student = array[i];
+    result.push(student.name);
+  }
+
+  return result;
+};
+
+console.log(namesFunction(studentsInfo));
+
+
+let namesReduce = studentsInfo.reduce(function (namesArr, student) {
+  namesArr.push(student.name);
+  return namesArr;
+}, []);
+
+console.log(namesReduce);
