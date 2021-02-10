c6e8cb4e0603318e22b0c9e11a820ee9c106c970
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-09 17:43:07 -0800

    add exercises for week 6

---
+++ MeetUps2021/week6.js
@@ -0,0 +1,35 @@
+// Fizz Buzz is a classic interview question that apparently many engineering candidates can't solve! Let's cover it today.
+
+// We're given a number in the form of an integer n.
+
+// Write a function that returns the string representation of all numbers from 1 to n based on the following rules:
+
+// If it's a multiple of 3, represent it as "fizz".
+
+// If it's a multiple of 5, represent it as "buzz".
+
+// If it's a multiple of both 3 and 5, represent it as "fizzbuzz".
+
+// If it's neither, just return the number itself.
+
+
+
+
+function fizzBuzz(n) {
+
+}
+
+console.log(fizzBuzz(5))
+console.log(fizzBuzz(3))
+console.log(fizzBuzz(7))
+
+
+
+
+function confirmEnding(str, target) {
+
+
+}
+confirmEnding("Bastian", "n");
+confirmEnding("Congratulation", "on")
+confirmEnding("He has to give me a new name", "name")
