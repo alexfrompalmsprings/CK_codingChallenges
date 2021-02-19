0cbbd2c3b00e159c34876981afab77674b84fea4
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-18 21:04:20 -0800

    complete return the largest number in the array

--- MeetUps2021/FCC/script.js
+++ MeetUps2021/FCC/script.js
@@ -60,8 +60,8 @@ function factorialize(num) {
   }
 
 }
-
 factorialize(5);
+
 
 
 // Find the Longest Word in a String
@@ -84,3 +84,17 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+
+
+// Return Largest Numbers in Arrays
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+function largestOfFour(arr) {
+
+
+
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
