f2f1c1b3b67458b46dde84668f8f345397892596
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-18 20:57:27 -0800

    complete longest word finder

--- MeetUps2021/FCC/script.js
+++ MeetUps2021/FCC/script.js
@@ -40,3 +40,47 @@ function reverseString(str) {
 }
 
 reverseString("hello");
+
+
+
+// Factorialize a Number
+// Return the factorial of the provided integer.
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+// Factorials are often represented with the shorthand notation n!
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+// Only integers greater than or equal to zero will be supplied to the function.
+
+function factorialize(num) {
+
+  // recursive function
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorialize(num - 1)
+  }
+
+}
+
+factorialize(5);
+
+
+// Find the Longest Word in a String
+// Return the length of the longest word in the provided sentence.
+// Your response should be a number.
+function findLongestWordLength(str) {
+
+  let words = str.split(' ');
+  let longest = words[0];
+
+  words.forEach(word => {
+    if (word.length >= longest.length) {
+      longest = word;
+    }
+  })
+
+
+
+  return longest;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
