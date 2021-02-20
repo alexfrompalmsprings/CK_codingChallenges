821d1d7c5c3cb0e56c6b29f61a9ae70d6341caff
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-19 21:52:52 -0800

    complete title case sentence

--- MeetUps2021/FCC/script.js
+++ MeetUps2021/FCC/script.js
@@ -185,3 +185,22 @@ function booWho(bool) {
 }
 
 booWho(null);
+
+
+// Title Case a Sentence
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+// For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+function titleCase(str) {
+  let result = [];
+  let words = str.toLowerCase().split(' ');
+
+  words.forEach(word => {
+    word = word[0].toUpperCase() + word.slice(1);
+    result.push(word)
+  })
+
+  return result.join(' ')
+
+}
+
+titleCase("I'm a little tea pot");
