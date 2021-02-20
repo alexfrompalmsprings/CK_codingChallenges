c28a1720e016796234168264384b0cbb8ad49443
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-19 21:33:32 -0800

    complete ~ truncate string
     Please enter the commit message for your changes. Lines starting

--- MeetUps2021/FCC/script.js
+++ MeetUps2021/FCC/script.js
@@ -149,3 +149,14 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+
+// Truncate a String
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+function truncateString(str, num) {
+  let sliced = str.slice(0, num);
+
+  return str.length <= num ? str : sliced + '...';
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
