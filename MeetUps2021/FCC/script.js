62e3a49caf6bdf7d4ea1069efbd4ddb22b378d00
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-19 22:00:27 -0800

    complete ~ slice & splice

--- MeetUps2021/FCC/script.js
+++ MeetUps2021/FCC/script.js
@@ -204,3 +204,13 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+
+// Slice and Splice
+// You are given two arrays and an index.
+
+// Copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
