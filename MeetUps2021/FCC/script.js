531c66aee6b6f6d9a956d56d293c46f615156f8d
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-19 21:39:07 -0800

    complete ~ finders keeper

--- MeetUps2021/FCC/script.js
+++ MeetUps2021/FCC/script.js
@@ -160,3 +160,17 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// Finders Keepers
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+function findElement(arr, func) {
+
+  let finder = arr.find(ele => {
+    return func(ele);
+  })
+
+  return finder;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
