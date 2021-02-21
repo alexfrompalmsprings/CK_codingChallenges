a42e0ed5f21e5fc5c17ce345081218ba97524183
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-21 12:49:25 -0800

    complete ~ diff two arrays

--- MeetUps2021/FCC/intermediateAlgo.js
+++ MeetUps2021/FCC/intermediateAlgo.js
@@ -5,6 +5,7 @@
 function sumAll(arr) {
 
   // ---- alexs code; a bit messy
+  // also could have used minn & max
   let sorted = arr.sort((a, b) => a - b);
   let sum = 0;
   let [first, second] = sorted;
@@ -20,3 +21,30 @@ function sumAll(arr) {
 }
 
 sumAll([1, 4]);
+
+
+// Diff Two Arrays
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+// Note
+// You can return the array with its elements in any order.
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  // ---- using two for eachs
+  arr1.forEach(ele => {
+    if (!arr2.includes(ele)) {
+      newArr.push(ele)
+    }
+  });
+
+  arr2.forEach(ele => {
+    if (!arr1.includes(ele)) {
+      newArr.push(ele)
+    }
+  });
+
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
