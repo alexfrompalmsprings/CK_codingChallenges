532a653fb8d2b3c907896b5e5783b41d2c84fe5b
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-27 12:58:14 -0800

    complete DNA pair

--- MeetUps2021/FCC/intermediateAlgo.js
+++ MeetUps2021/FCC/intermediateAlgo.js
@@ -251,4 +251,40 @@ function myReplace(str, before, after) {
 
 }
 
-console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "Leaped"));
+// console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "Leaped"));
+
+
+// DNA Pairing
+// The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+// Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+// Return the provided character as the first element in each array.
+
+// For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+// The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+function pairElement(str) {
+  let pairs = {
+    A: 'T',
+    T: 'A',
+    G: 'C',
+    C: 'G',
+  }
+
+
+  let letters = str.split('');
+  console.log(letters);
+
+  let result = letters.map(letter => {
+    let pair = [letter, pairs[letter]];
+
+
+    return pair;
+  })
+
+
+  return result;
+}
+
+console.log(pairElement("GCG"));
