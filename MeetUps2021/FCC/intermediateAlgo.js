f2637fca3ed3a3e62352a5af9b884ae36789a598
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-02-27 14:21:00 -0800

    complete uniteUnique

--- MeetUps2021/FCC/intermediateAlgo.js
+++ MeetUps2021/FCC/intermediateAlgo.js
@@ -319,11 +319,38 @@ function fearNotLetter(str) {
   })
 
   return filtered.length > 0 ? filtered.join('') : undefined;
+}
+// console.log(fearNotLetter("efgij"));
 
 
+// Sorted Union
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+// Check the assertion tests for examples.
+
+function uniteUnique(...arr) {
+
+  let arrays = [...arr];
+
+  let result = [];
+
+  arrays.forEach(array => {
+    for (let i = 0; i < array.length; i++) {
+      let num = array[i];
+
+      if (!result.includes(num)) {
+        result.push(num);
+        console.log(result)
+      }
+    }
+  })
 
 
-
+  return result;
 }
 
-// console.log(fearNotLetter("efgij"));
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
