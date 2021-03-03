ce8cb0da0d87870c056b21f1c57b2e07f58c210a
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-03-02 21:42:09 -0800

    add questions week 2 fcc

--- MeetUps2021/FreeCodeCamp/basicAlgo.js
+++ MeetUps2021/FreeCodeCamp/basicAlgo.js
@@ -196,4 +196,113 @@ function largestOfFour(arr) {
   return result;
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+// console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+
+
+// Confirm the EndingPassed
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+// This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+function confirmEnding(str, target) {
+
+  /*
+  output ~  boolean (true/false)
+  input ~ one 'string' & target 'str'
+  */
+
+  // suggestion #FCC - use a substring
+  // with the substring compare to the target
+  // true/ false
+
+  // zero index ~
+  //0123456
+  // "Bastian"
+  // 0 1 2 3 4 5 6 7
+  // ['b a s t i a n]
+  //6
+  // [ c o n g r a t s]
+
+  // how do get the start index and end index
+  // 7 -1  = 6
+  // 8 -2 = 6
+
+  let startIndex = str.length - target.length;
+  console.log(startIndex);
+
+
+  // create a substring to check the target
+  let endingStr = str.slice(startIndex);
+
+
+  // // return true/false
+  // if (endingStr === target) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+
+  return endingStr === target; // true or false
+
+
+}
+
+
+// console.log(confirmEnding("Bastian", "n"));
+// console.log(confirmEnding("CongratsOnYourBday", "on")); // true or false
+
+
+
+// Repeat a String Repeat a StringPassed
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+function repeatStringNumTimes(str, num) {
+
+  // input  ~ ( str & num)
+  // output ~ string
+
+  // examples ~
+
+  // suggestions & ideas
+
+  // create an empty string
+
+
+
+  let result = '';
+  let arr = []; // an empty array []
+
+  // if the num is not positive return an empty string
+  if (num < 1) {
+    return result
+  }
+
+  // using a foor loop
+  // starting point;  -- NUM (3)
+  // condition to end;  -- (0)
+  // increment it / reduce it | Num -1
+
+  for (let i = num; i > 0; i = i - 1) {
+
+    console.log(i)
+
+    arr.push(str); //   ['abc ,abc, abc'] - not memory efficient | data structure takes more space
+    // console.log(arr);
+    result += str;
+    // console.log(result)
+
+
+  }
+
+  console.log(arr); // ['abc ,abc, abc']
+  console.log(arr.join('')); // changing to the string
+
+  // arr = 25;
+  // console.log(arr);
+
+  //return result
+  return result;
+}
+
+console.log(repeatStringNumTimes("abc", 3));
